c117e8e04d73d1171babbd6e1e6d5da813c0a23d
Yashas <yashasjob@gmail.com>
2024-11-01 16:36:52 +0530

    commit

---
+++ src/components/MarkdownPreview.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MarkdownPreview = ({ html }) => {
+  return <div className="preview" dangerouslySetInnerHTML={{ __html: html }} />;
+};
+
+export default MarkdownPreview;
